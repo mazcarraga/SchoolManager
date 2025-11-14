d4c55c1b2d0930a8c2c4af228d5850dbfcaaa4a6
Ma. willah Ederlyn M. Saturius <msaturius@gbox.adnu.edu.ph>
2025-11-15 07:26:50 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "home/index"
+  root "home#index"
   resources :relationships
   resources :guardians
   resources :staffs
@@ -19,5 +21,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # 
 end
