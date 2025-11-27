3290f04e73ccb5bdbd77aba8c1ffe6a608f447d0
Maria Socorro Azcarraga <mazcarraga@gbox.adnu.edu.ph>
2025-11-27 13:54:11 +0800

    About Us

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :teachers
   resources :students
   resources :departments
+
+  get "about", to: "home#about_us"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
